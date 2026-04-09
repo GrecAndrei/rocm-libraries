f932da226e48bf4c9a9a41630a94e8df44df6579
Brent Maas <brent.maas@streamhpc.com>
2026-04-09 13:20:59 +0000

    Generated frontend code after fixing code generation mistakes

--- projects/hipdnn/backend/include/HipdnnBackendDescriptorType.h
+++ projects/hipdnn/backend/include/HipdnnBackendDescriptorType.h
@@ -304,4 +304,11 @@ typedef enum
      */
     HIPDNN_BACKEND_OPERATION_RMSNORM_BACKWARD_DESCRIPTOR_EXT = 33,
 
+    /**
+     * @brief Layernorm backward operation descriptor
+     *
+     * Represents a backward layer normalization operation with output gradient (DY), input (X), scale, mean, inverse variance, input gradient (DX), scale gradient and mean gradient tensors, a backward layernorm operator, and a compute data type
+     */
+    HIPDNN_BACKEND_OPERATION_LAYERNORM_BACKWARD_DESCRIPTOR = 34,
+
 } hipdnnBackendDescriptorType_t;
