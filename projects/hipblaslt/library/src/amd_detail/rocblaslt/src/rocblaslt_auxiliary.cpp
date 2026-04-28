e1925f15c8e97722197b8fcc74f281395840a044
Seung-Hee Bae <Seung-Hee.Bae@amd.com>
2026-04-28 22:19:01 +0000

    minor fix of the type of an internal function name.

--- projects/hipblaslt/library/src/amd_detail/rocblaslt/src/rocblaslt_auxiliary.cpp
+++ projects/hipblaslt/library/src/amd_detail/rocblaslt/src/rocblaslt_auxiliary.cpp
@@ -590,7 +590,7 @@ rocblaslt_status rocblaslt_destroy(const rocblaslt_handle handle)
  * content. It must be initialized using rocblaslt_matrix_layout_create()
  * and the retured handle must be passed
  * to all subsequent library function calls that involve the matrix.
- * It should be destroyed at the end using rocblaslt_matrix_layout_destory().
+ * It should be destroyed at the end using rocblaslt_matrix_layout_destroy().
  *******************************************************************************/
 rocblaslt_status rocblaslt_matrix_layout_create(rocblaslt_matrix_layout* matDescr,
                                                 hipDataType              valueType,
@@ -638,7 +638,7 @@ rocblaslt_status rocblaslt_matrix_layout_create(rocblaslt_matrix_layout* matDesc
 /********************************************************************************
  * \brief destroy matrix descriptor
  *******************************************************************************/
-rocblaslt_status rocblaslt_matrix_layout_destory(const rocblaslt_matrix_layout matDescr)
+rocblaslt_status rocblaslt_matrix_layout_destroy(const rocblaslt_matrix_layout matDescr)
 {
     if(matDescr == nullptr)
     {
