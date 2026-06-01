da77be845c600faf9204827f4ed02874277f36ee
Seung-Hee Bae <Seung-Hee.Bae@amd.com>
2026-06-01 01:16:31 +0000

    [hipblaslt] fix bugs in the changes of KernelWriterConversion.py

--- projects/hipblaslt/tensilelite/src/ContractionSolution.cpp
+++ projects/hipblaslt/tensilelite/src/ContractionSolution.cpp
@@ -2055,11 +2055,12 @@ namespace TensileLite
             args.template append<void const* const*>("batchC", inputs.batchC);
 
         // Pass batch offsets when kernel expects them (SupportUserArgs=true, not GroupedGemm)
-        if(problemType.supportDeviceUserArguments && !problemType.groupedGemm)
-        {
-            args.template append<int64_t>("batchOffsetD", inputs.batchOffsetD);
-            args.template append<int64_t>("batchOffsetC", inputs.batchOffsetC);
-        }
+        //if(problemType.supportDeviceUserArguments && !problemType.groupedGemm)
+//        if(!problemType.groupedGemm)
+//        {
+//            args.template append<int64_t>("batchOffsetD", inputs.batchOffsetD);
+//            args.template append<int64_t>("batchOffsetC", inputs.batchOffsetC);
+//        }
 
         bool useBias = false;
         if(problemType.useBias)
@@ -2213,6 +2214,9 @@ namespace TensileLite
             ContractionProblemGemm::BATCHMODE batchMode = problem.batchMode();
             args.template append<uint32_t>("batchMode", static_cast<uint32_t>(batchMode));
             args.template append<uint32_t>("additionalPaddingPerBatch", additionalPaddingPerBatchGeneralBatch);        
+
+            args.template append<int64_t>("batchOffsetD", inputs.batchOffsetD);
+            args.template append<int64_t>("batchOffsetC", inputs.batchOffsetC);
         }
     }
 
