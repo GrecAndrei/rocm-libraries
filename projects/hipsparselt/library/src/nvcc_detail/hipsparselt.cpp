8a1ff09deb93f8ab947d2bb21591c10418fbd693
Vin Huang <vin.huang@amd.com>
2026-06-08 14:04:57 +0000

    [hipSPARSELt] Add hipsparseLtMatmulAlgSelectionDestroy() API to release algorithm selection descriptor resources

--- projects/hipsparselt/library/src/nvcc_detail/hipsparselt.cpp
+++ projects/hipsparselt/library/src/nvcc_detail/hipsparselt.cpp
@@ -717,6 +717,12 @@ hipsparseStatus_t
                                          HIPMatmulAlgToCuSparseLtMatmulAlg(alg)));
 }
 
+hipsparseStatus_t hipsparseLtMatmulAlgSelectionDestroy(const hipsparseLtMatmulAlgSelection_t* algSelection)
+{
+    return hipCUSPARSEStatusToHIPStatus(
+        cusparseLtMatmulAlgSelectionDestroy((const cusparseLtMatmulAlgSelection_t*)algSelection));
+}
+
 hipsparseStatus_t hipsparseLtMatmulAlgSetAttribute(const hipsparseLtHandle_t*       handle,
                                                    hipsparseLtMatmulAlgSelection_t* algSelection,
                                                    hipsparseLtMatmulAlgAttribute_t  attribute,
