1ba1c316a8198f28c5be96cbd9d6bf209c37c3fd
Samuel Reeder <samuel.reeder@amd.com>
2026-06-08 21:05:56 +0000

    Remove HIP runtime binding aliases

--- projects/hipdnn/python/hipdnn_frontend/__init__.py
+++ projects/hipdnn/python/hipdnn_frontend/__init__.py
@@ -79,12 +79,4 @@ __all__ = [
     "hip_stream_synchronize",
     "hip_device_synchronize",
     "hip_get_device_count",
-    "hipEventCreate",
-    "hipEventDestroy",
-    "hipEventRecord",
-    "hipEventSynchronize",
-    "hipEventElapsedTime",
-    "hipStreamSynchronize",
-    "hipDeviceSynchronize",
-    "hipGetDeviceCount",
 ]
